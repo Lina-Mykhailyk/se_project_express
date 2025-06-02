9f0bd37a204a04ed9054bae9a7cf5807f53a8c31
Lina Mykhailyk <mykhailyk.anhelina@gmail.com>
2025-06-02 12:36:00 +0300

    Small fix after testing s13 (2)

--- controllers/users.js
+++ controllers/users.js
@@ -13,6 +13,12 @@ const { JWT_SECRET } = require("../utils/config");
 const login = (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res
+      .status(BAD_REQUEST)
+      .send({ message: "Email and password are required." });
+  }
+
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
