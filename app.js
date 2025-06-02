ac383f6c13638d0e251c07e1915a94cedbce1b50
Lina Mykhailyk <mykhailyk.anhelina@gmail.com>
2025-06-02 11:59:04 +0300

    Small fix after testing s13 (1)

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
 const { NOT_FOUND } = require("./utils/errors");
-const auth = require("./middlewares/auth");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -18,8 +17,6 @@ mongoose
 app.use(cors());
 
 app.use(express.json());
-
-app.use(auth);
 
 app.use("/", mainRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const { createUser, login } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
 router.post("/signin", login);
 router.post("/signup", createUser);
+router.get("/items", require("../controllers/clothingItems").getItems);
+
+router.use(auth);
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
